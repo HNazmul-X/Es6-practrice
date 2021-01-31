9c640583c417f64509f2bda0b511a6a720d7b5d6
H Nazmul Hassan <71649134+HNazmul@users.noreply.github.com>
2021-02-01 01:20:22 +0600

    some file added

---
+++ file-01/index.js
@@ -0,0 +1,4 @@
+function sum(num1 = 0, num2= 0){
+    return num1 + num2
+}
+console.log(sum("rakib ","sabina"))
---
+++ file-01/tamplate.js
@@ -0,0 +1,11 @@
+ let name1 = "Nazmul "
+ let name2 = "Sabina "
+ let number1 = 20;
+
+ function getNumber (){
+    return 40;
+ }
+ let number2 = `${number1}`
+ console.log(typeof number2);
+
+ console.log(`${name1+ name2} ${name1 + name2} ${getNumber()}`)
